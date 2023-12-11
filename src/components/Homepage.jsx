8ea9782fd421906d65fa0217658154852ec8d8e6
accelerator-01 <acceleratorgame01@gmail.com>
2023-12-11 20:34:12 +0700

    .

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,10 +2,16 @@ import NavBar from "../ui/NavBar";
 
 function Homepage() {
   return (
-    <div>
+    <div className="">
       <NavBar />
-      <div className="text-center">
-        <p>Home</p>
+      <div>
+        <h1 className="text-center text-[100px] text-slate-100">Test</h1>
+        <h3 className="text-md text-center text-slate-100">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam omnis
+          quia laboriosam dolore quisquam neque ullam ex quaerat. Impedit,
+          voluptatum? Ea, fugiat reiciendis! Quisquam magnam officia ea!
+          Officiis, molestiae quos.
+        </h3>
       </div>
     </div>
   );
--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -5,10 +5,10 @@ function NavBar() {
     <nav className="bg-gray-800">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center">
-            <button type="button" className="button">
-              <Link to="/">Home</Link>
-            </button>
+          <button type="button" className="button">
+            <Link to="/">Home</Link>
+          </button>
+          <div className="inset-y-0 left-0 flex">
             <button className="button">
               <Link to="/portfolio">Project</Link>
             </button>
