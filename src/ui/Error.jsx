122c6285ce33c5dd41882665f616f2d311f641e8
accelerator-01 <acceleratorgame01@gmail.com>
2024-01-17 23:14:45 +0700

    .

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,21 +1,10 @@
-import { useRouteError } from "react-router-dom";
-
 function Error() {
-  const error = useRouteError();
-
   return (
-    <div>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            Page not found
-          </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
-            {error.data || error.message}
-          </p>
-        </div>
-      </main>
+    <div className="text-blue-gray-900 mx-auto py-10 text-center">
+      <h1 className="text-[50px] font-extrabold">Error 404</h1>
+      <p className="text-[40px] font-semibold">
+        The page you are looking for is not available
+      </p>
     </div>
   );
 }
