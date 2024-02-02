c7fd5b02997dc140ce0122cba0408b9d9f1887f2
accelerator-01 <acceleratorgame01@gmail.com>
2024-02-02 23:04:20 +0700

    .

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,24 +1,46 @@
+import { useState } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+
 function Portfolio() {
+  const [open, setOpen] = useState(1);
+
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);
+
   return (
-    <div className="container mx-auto border text-center">
+    <div className="container mx-auto border">
       <h1 className="text-bold text-center font-mono text-white underline">
         Projects:
       </h1>
-      <div className="mb-3 flex">
-        <div className="flex-wrap">
-          <h1 className="ml-3 font-serif text-lg text-white">
+      <Accordion
+        className="border-blue-gray-100 mb-2 rounded-lg border px-4"
+        open={open === 0}
+      >
+        <AccordionHeader
+          onClick={() => handleOpen(1)}
+          className="ml-3 justify-between font-serif text-lg text-white"
+        >
+          <a
+            className="hover:text-blue-500 hover:underline"
+            href="https://fast-react-pizza-blond.vercel.app/"
+            target="_blank"
+          >
             Fast React Pizza Co.
-          </h1>
-          <a href="https://fast-react-pizza-blond.vercel.app/" target="_blank">
-            <img
-              className="mb-3 h-full w-full object-contain"
-              src="/Fast-React-Pizza-Co.jpeg"
-            />
           </a>
-        </div>
-      </div>
+          <span className="">Click here to see the preview</span>
+        </AccordionHeader>
+        <AccordionBody>
+          <img
+            className="mb-3 h-full w-full object-contain"
+            src="/Fast-React-Pizza-Co.jpeg"
+          />
+        </AccordionBody>
+      </Accordion>
 
-      <div className="mb-3 flex">
+      {/* <div className="mb-3 flex">
         <div className="flex-wrap">
           <h1 className="ml-3 font-serif text-lg text-white">
             World Wise Project
@@ -46,7 +68,7 @@ function Portfolio() {
             />
           </a>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
