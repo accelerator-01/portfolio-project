731a2c91ebe6da6b2e7097e856c0e84be3f54cb7
accelerator-01 <acceleratorgame01@gmail.com>
2024-02-29 21:22:48 +0700

    edit screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import Cert from "./components/Cert";
 
 export default function App() {
   return (
-    <div className="container mx-auto lg:grid lg:grid-cols-2 lg:gap-x-1">
+    <div className="container mx-auto md:grid md:grid-cols-2 md:gap-x-1 lg:grid lg:grid-cols-2 lg:gap-x-1">
       <div className="m-3 overflow-hidden p-4">
         <Profile />
         <About />
-        <Skill />
+        <div className="sm:hidden">
+          <Skill />
+        </div>
       </div>
       <div className="m-3 overflow-hidden p-4">
         <Portfolio />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.jsx"],
   theme: {
+    screens: {
+      sm: { min: "320px", max: "760px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "760px", max: "1023px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "1024px", max: "1400px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+    },
     extend: {},
   },
   plugins: [],
